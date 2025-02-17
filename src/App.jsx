5363709295f02c58feeaca2df089d829d5f4db7b
DariusBere <dariusbere8@gmail.com>
2025-02-17 14:21:07 +0100

    The general Idea of the Site

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,58 @@ import './App.css'
 
 function App() {
 
+  /*This will be the main page of this site
+    You will need to have:
+      - Index0
+      - Gallery 
+      - Contact & Pricing
+    You may want to approach this is a bunch of different ways, 
+    but the main theme of this site will be online food magazine where you can periodically order the ingredients of different recipes to your house
+    
+    Depending on how frequent you want to have meals delivered to you door, the price will change into the Pricing Page. 
+    Main parameters of this pricing page will be 
+    - Frequency (days, weeks, months)
+    - Portions (1 person, 2 people, 4, 6...)
+    - Level of product (Basic, Premium, or Platinum)
+    
+    To receive the final quote and start the subscription you will have to enter the main contact detail of the user that are :
+    - Name -> ONLY letters with a maximum of 15 characters
+    - Surname -> ONLY letters with a maximum of 40 characters
+    - Phone number -> ONLY numbers with a maximum of 9 digits
+    - E-mail -> Has to validate the standards of an email -> abcd_efg@123abc.com 
+
+    ^^^ All this form inputs need to be verified and valid with JavaScript validation, the form will not be submitted until all form inputs are valid
+
+
+
+
+
+    There will be a Contact and Customer Service page where the user will be able to tell us about his/her experience, some good reviews will be posted below the form
+    as example ->
+      "Brittany - 5 stars 
+        - Amazing customer service, I had a minor inconvenience and they made sure they will get rid of it, really high quality service."
+    
+      "John - 4.5 stars 
+        - Really great food, amazing service and fast delivery times. 
+        Would prefer more options to choose from, I find myself eating the same stuff time and time again."
+    
+      "Esmeralda - 5 stars 
+        - This is the best service money can buy for a single mother like me. 
+        Right as I come back from work it barely takes any time for the ingredients to arrive so that I can prepare my little angels their favorite meals."
+     
+    The form elements are the following: 
+      - Name and Surname
+      - Age
+      - E-mail
+      - Rating
+      - Review Title
+      - Review 
+
+
+  */
+  return ()
 
   
-  return (
-    
-    
-  )
 }
 
 export default App
